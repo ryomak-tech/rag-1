866c8edc92e8253ad99e7917f6357a4f762fddd9
RYOMA <ryoma@RYOMAs-MacBook-Air.local>
2025-06-16 19:03:41 +0900

    modify pdf import

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import time
 
 # LangChain / Transformers
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoaderfrom langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
